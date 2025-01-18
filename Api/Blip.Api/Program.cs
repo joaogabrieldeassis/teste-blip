209a7934e6f255d4b590328243e664bfe61745a6
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2025-01-18 19:15:40 -0300

    Ajuste no ambiente

--- Api/Blip.Api/Program.cs
+++ Api/Blip.Api/Program.cs
@@ -15,11 +15,10 @@ builder.Services.AddHttpClient<GithubService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
